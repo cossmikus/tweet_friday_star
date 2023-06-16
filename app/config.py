0129d1c3035fe15a7ed171b9263ec83d9de4a01b
cossmikus <kassymkhan.bolat@nu.edu.kz>
2023-06-16 18:01:35 +0600

    zebra

--- app/config.py
+++ app/config.py
@@ -1,11 +1,9 @@
 from typing import Any
-
 from pydantic import BaseSettings
 from pymongo import MongoClient
 
 
 class Config(BaseSettings):
-    
     CORS_ORIGINS: list[str] = ["*"]
     CORS_HEADERS: list[str] = ["*"]
     CORS_METHODS: list[str] = ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"]
@@ -16,18 +14,19 @@ class Config(BaseSettings):
     MONGOPASSWORD: str = "password"
     MONGODATABASE: str = "fastapi"
     MONGO_URL: str = ""
+    HERE_API_KEY: str = ""  # Add this line
 
 
-# environmental variables
+# Environmental variables
 env = Config()
 
 # FastAPI configurations
 fastapi_config: dict[str, Any] = {
     "title": "API",
 }
-mongo_url = (
-    f"mongodb://{env.MONGOUSER}:{env.MONGOPASSWORD}@{env.MONGOHOST}:{env.MONGOPORT}/"
-)
+
+mongo_url = f"mongodb://{env.MONGOUSER}:{env.MONGOPASSWORD}@{env.MONGOHOST}:{env.MONGOPORT}/"
+
 if env.MONGO_URL:
     mongo_url = env.MONGO_URL
 
